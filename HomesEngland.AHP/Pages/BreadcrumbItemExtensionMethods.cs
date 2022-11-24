35eadb17e4ee978bdeddf09363139e7f77af0c08
Daniel Symonds-Lloyd <daniel@wandle.solutions>
2022-11-24 14:49:20 +0000

    Breadcrumbs for admin

---
+++ HomesEngland.AHP/Pages/BreadcrumbItemExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using HomesEngland.AHP.Data;
+using HomesEngland.AHP.Shared;
+
+namespace HomesEngland.AHP.Pages;
+
+public static class BreadcrumbItemExtensionMethods
+{
+	public static List<BreadcrumbItem> AddAdminFeatures(this List<BreadcrumbItem> breadcrumbItems)
+	{
+		breadcrumbItems.Add(new BreadcrumbItem($"/admin/features", "Features"));
+		return breadcrumbItems;
+	}
+
+	public static List<BreadcrumbItem> AddAdminProgramme(this List<BreadcrumbItem> breadcrumbItems, Programme programme)
+	{
+		breadcrumbItems.Add(new BreadcrumbItem($"/admin/programmes/byid/{programme.ProgrammeId}", programme.ProgrammeName));
+		return breadcrumbItems;
+	}
+
+	public static List<BreadcrumbItem> AddAdminProgrammes(this List<BreadcrumbItem> breadcrumbItems)
+	{
+		breadcrumbItems.Add(new BreadcrumbItem("/admin/programmes", "Programmes"));
+		return breadcrumbItems;
+	}
+}
---
+++ HomesEngland.AHP/Shared/BreadcrumbItem.cs
@@ -0,0 +1,51 @@
+﻿namespace HomesEngland.AHP.Shared;
+
+public class BreadcrumbItem : IEquatable<BreadcrumbItem>
+{
+	public BreadcrumbItem()
+	{
+
+	}
+
+	public BreadcrumbItem(string href, string title)
+	{
+		Href = href;
+		Title = title;
+	}
+
+	public string Href { get; set; }
+
+	public string Title { get; set; }
+
+	public static List<BreadcrumbItem> AdminBase() => new()
+	{
+		new BreadcrumbItem("/", "P.A.N.O.S"),
+		new BreadcrumbItem("/admin", "Admin"),
+	};
+
+	public static List<BreadcrumbItem> Home() => new()
+	{
+		new BreadcrumbItem("/", "P.A.N.O.S"),
+	};
+
+	public bool Equals(BreadcrumbItem? other)
+	{
+		if (this == null && other == null)
+		{
+			return true;
+		}
+
+		if (this == null || other == null)
+		{
+			return false;
+		}
+
+		return this.Title == other.Title
+			&& this.Href == other.Href;
+	}
+
+
+	public override bool Equals(object obj) => Equals(obj as BreadcrumbItem);
+	public override int GetHashCode() => (Href, Title).GetHashCode();
+}
+
