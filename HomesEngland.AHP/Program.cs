bf9df9b89ceff76025487b78729e06905833497b
Daniel Symonds-Lloyd <daniel@wandle.solutions>
2022-12-02 19:30:03 +0000

    Publish profiles for Azure and bugfixes

--- HomesEngland.AHP/Program.cs
+++ HomesEngland.AHP/Program.cs
@@ -20,11 +20,16 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddScoped<IGrantRepository, SqlGrantRepository>();
 builder.Services.AddScoped<MilestoneCreationService>();
 
+builder.Services.AddResponseCompression();
+builder.Services.AddApplicationInsightsTelemetry();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+	app.UseResponseCompression();
 	app.UseExceptionHandler("/Error");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
